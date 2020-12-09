9141433c72f6d9b5c383fed507125aacc4e63d76
WilsonJunior85 <69021635+WilsonJunior85@users.noreply.github.com>
2020-12-08 21:34:59 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var menu1 = document.querySelector(".menu1");     /* criando uma variavel e buscando o menu1 */
+
+/* Adicionar o evento addEventListener que vai ficar escutando o evento de click */
+menu1.addEventListener("click", function() {
+/* Criando uma variável com elemento sidebar, apartir da lista de classe add ou remover uma classe chamada show-menu */
+var sidebar = document.querySelector(".container").classList.toggle("show-menu");
+
+
+});
